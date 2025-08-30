8d3d74c3343230364e8b7e263d51f17751768e4f
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-08-30 16:45:14 +0800

    removed sections

--- src/pages/home/section/index.ts
+++
@@ -1,6 +0,0 @@
-export { AppAboutMe } from "./AppAboutMe";
-export { AppGallery } from "./AppGallery";
-export { AppHero } from "./AppHero";
-export { AppMyExperience } from "./AppMyExperience";
-export { AppProjects } from "./AppMyProjects";
-
