cce99ad6cc3776f2f98901b9118b0a82efbd66f3
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-08-30 23:50:52 +0800

    for deployment

--- src/components/common/LizardComponents/LizardHeader.tsx
+++ src/components/common/LizardComponents/LizardHeader.tsx
@@ -1,27 +1,27 @@
 
-import { LizardAndTitleLogo } from ".";
+import { LizardAndTitleLogo, LizardDiv } from ".";
 import { LizardDateAndTime } from "./LizardDateAndTime";
 import { LizardLogoContainer } from "./LizardLogoContainer";
 
 export function LizardHeader() {
 
     return (
-        <div className="flex w-full items-center gap-4 bg-black">
-            <div className="flex w-full justify-between items-center">
+        <LizardDiv className="flex w-full items-center gap-4 bg-black">
+            <LizardDiv direction="row" className="flex w-full justify-between items-center">
                 {/* Logo container */}
-                <div className="px-6 md:px-10 lg:px-20 py-2 flex w-full lg:w-auto justify-center">
+                <LizardDiv className="px-6 md:px-10 lg:px-20 py-2 flex w-full items-center lg:w-auto justify-center">
                     <LizardLogoContainer
                         svg={LizardAndTitleLogo}
-                        className="w-40 sm:w-50 lg:60 h-auto fill-[#88fb59ff] stroke-[#88fb59ff]"
+                        className="w-40 sm:w-50 lg:60 h-auto  stroke-[#88fb59ff]"
                     />
-                </div>
+                </LizardDiv>
 
                 {/* Right content (hidden on small screens) */}
-                <div className="hidden lg:flex gap-4 md:px-10 lg:px-20">
+                <LizardDiv direction="row" className="hidden lg:flex gap-4 md:px-10 lg:px-20">
                     <LizardDateAndTime time="utcTime" type="time" label="Server Time:" />
                     <LizardDateAndTime time="localTime" type="time" label="Local Time:" />
-                </div>
-            </div>
-        </div>
+                </LizardDiv>
+        </LizardDiv>
+        </LizardDiv>
     );
 }
