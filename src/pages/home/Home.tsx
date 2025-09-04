ed44b6dd7078b744856a4fdc4c6cf04a612060e6
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-09-04 10:06:37 +0800

    adjusted indention

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -11,8 +11,6 @@ export default function Home() {
 
   return (
     <LizardDiv direction='row' className="relative flex w-full flex-1 mx-auto overflow-hidden justify-center items-center">
-
-
       <LizardDiv className="absolute top-1/2 left-1/2 z-0 h-[450px] w-full -translate-x-1/2 -translate-y-1/2 ">
         <LizardAnimatedBackground
           svg={Center}
@@ -22,10 +20,6 @@ export default function Home() {
       <LizardDiv className="relative z-10">
        <LizardControlButton/>
       </LizardDiv>
-
-
-
-
     </LizardDiv>
   )
 }
