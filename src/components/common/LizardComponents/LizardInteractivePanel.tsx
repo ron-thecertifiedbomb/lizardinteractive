a0607fc647c7f70ea2a17d7982f067adfe2daaec
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-09-04 15:32:41 +0800

    fixed root styling

--- src/components/common/LizardComponents/LizardInteractivePanel.tsx
+++ src/components/common/LizardComponents/LizardInteractivePanel.tsx
@@ -51,12 +51,11 @@ export function LizardInteractivePanel({
 
   return (
     <LizardDiv direction="row"
-      className={`overflow-hidden inline-flex justify-center gap-1 lg:gap-4 transition-transform duration-500 ease-in-out
-        ${disabled ? "relative pointer-events-none translate-y-0" : showPanel ? "translate-y-0" : "translate-y-full pointer-events-none"}
+      className={`overflow-hidden inline-flex justify-center gap-1 lg:gap-4 transition-transform duration-500 ease-in-out 
+
         ${positionClassName}
       `}
     >
-
 
       {panelsToRender.map((panel) => {
         const isDisabledCard = "items" in panel;
@@ -65,49 +64,48 @@ export function LizardInteractivePanel({
         return (
           <LizardDiv
             key={panel.heading}
-            className={`min-h-0 h-auto box-content rounded-sm flex flex-col pl-1 lg:pl-2 transition-colors duration-300 ease-in-out
+            className={`h-auto box-content rounded flex flex-col pl-1 lg:pl-2 transition-colors duration-300 ease-in-out 
     ${disabled || isDisabledCard
-                ? "cursor-not-allowed bg-[#E84A4A] text-gray-400"
+                ? "cursor-not-allowed bg-[#E84A4A] text-[#049851]"
                 : "cursor-pointer hover:opacity-[0.8] hover:shadow-lg"}
     ${!disabled && !isDisabledCard && isActive
-                ? "bg-[#E84A4A] text-white"
+                ? "bg-[#E84A4A]  border"
                 : !disabled && !isDisabledCard
-                  ? "bg-[#242425] hover:bg-white/10"
+                  ? "bg-black/20 hover:bg-white/10  border"
                   : ""}
     ${cardClassName || ""}
   `}
             style={{
               background: !disabled && !isDisabledCard && isActive
                 ? "radial-gradient(125% 125% at 50% 90%, #000000 40%, #072607 100%)"
-                : "radial-gradient(circle at top, #1c1c1c, #000000)",
+
+                : "transparent",
             }}
             onClick={() => {
               if (disabled || isDisabledCard) return;
 
               setSection(panel.heading as keyof AppData);
               setActivePanel(panel.key);
-              // if (isMobile) {
-              //   setShowPanel(false);
-              // }
+
             }}
           >
             {/* Header */}
             <LizardDiv className="w-full relative overflow-hidden">
               {isDisabledCard ? (
                 heading && (
-                  <LizardDiv className="text-[14px] lg:text-[25px] pl-1 lg:pl-3 py-1 text-white font-light">
-                    <LizardText variant="h1">{heading.toUpperCase()}</LizardText>
+                  <LizardDiv className="text-[14px] lg:text-[25px] pl-1 lg:pl-3 py-1 text-[#049851] font-light">
+                    <LizardText variant="h3">{heading.toUpperCase()}</LizardText>
                   </LizardDiv>
                 )
               ) : (
-                <LizardDiv direction="row" className="text-[12px] w-full justify-between sm:text-[18px] lg:text-[20px] sm:pl-2 pl-1 pr-1 lg:pl-2 lg:pr-3 py-1 text-white font-light ">
+                <LizardDiv direction="row" className="text-[12px] w-full justify-between sm:text-[18px] lg:text-[20px] sm:pl-2 pl-1 pr-1 lg:pl-2 lg:pr-3 py-1 text-[#049851] font-light ">
                   <LizardText variant="h1">{panel.heading.toUpperCase()}</LizardText>
                   <LizardLogoContainer
                     svg={Lizard}
                     logoStroke="stroke-[#ffffff] stroke-[5]"
                     logoHeight="h-4"
                     logoWidth="w-4"
-                    logoFill="fill-[#b3b3b3]"
+                      logoFill="fill-[#ffffff]"
                     className="opacity-80"
                   />
 
@@ -122,7 +120,7 @@ export function LizardInteractivePanel({
 
             {/* Body */}
             {isDisabledCard ? (
-              <LizardDiv className="bg-transparent flex-1 flex flex-col px-3 py-2 min-h-[100px]">
+              <LizardDiv className="bg-[#2E4F32] flex-1 flex flex-col px-3 py-2 min-h-[100px]">
                 {panel.items.map((item, index) => (
                   <LizardDiv key={index} className="mb-3">
                     <LizardText
@@ -134,7 +132,7 @@ export function LizardInteractivePanel({
                     </LizardText>
                     <LizardText
                       variant="p"
-                      className={`text-[16px] uppercase text-[#E84A4A] w-full leading-none ${item.valueClassName || "mb-1"} ${item.valueProps?.className || ""}`}
+                      className={`text-[16px] uppercase text-[#E84A4A] w-full leading-none ${item.valueClassName || ""} ${item.valueProps?.className || ""}`}
                       {...item.valueProps}
                     >
                       {item.value}
@@ -143,11 +141,11 @@ export function LizardInteractivePanel({
                 ))}
               </LizardDiv>
             ) : (
-              <LizardDiv className="bg-black/40 flex-1 flex px-1 lg:px-2  border-t-1 border-l-1 rounded-sm">
+              <LizardDiv className="bg-transparent flex-1 flex px-1 lg:px-2  border-t border-l rounded">
                 {"overview" in panel && (
                   <LizardText
                     variant="p"
-                    className="text-[8px] sm:text-[12px] lg:text-[14px] p-1 sm:p-2 lg:p-3 text-[#b3b3b3] font-light"
+                    className="text-[8px] sm:text-[12px] lg:text-[14px] p-1 sm:p-2 lg:p-3  "
                   >
                     {panel.overview.toUpperCase()}
                   </LizardText>
